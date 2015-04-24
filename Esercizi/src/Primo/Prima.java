d30b9d1ba1741853282fd2f20f364a02dd7a587b
KonradHerba <k_herba@wp.pl>
2015-04-24 20:19:30 +0200

    First commit

---
+++ Esercizi/src/Primo/Prima.java
@@ -0,0 +1,25 @@
+package Primo;
+
+import java.util.Scanner;
+
+public class Prima {
+    public static void main(String[] args) {
+    	int liczba;
+    	try {
+    		System.out.println("Podaj liczbe calkowita: ");
+    		Scanner tekst = new Scanner(System.in);
+    		liczba = Integer.parseInt(tekst.nextLine());
+    		tekst.close();
+    		}
+
+        catch(NumberFormatException nfe) {
+        	System.out.println("Zly format!");
+        	liczba = 0;
+        	}
+
+    	while (liczba > 0){
+    		System.out.println(liczba);
+    		liczba = 0;
+    	}
+   	}
+}
---
+++ Esercizi/src/Primo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Konrad
+ *
+ */
+package Primo;
